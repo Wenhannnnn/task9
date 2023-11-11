639f28e888ff01aa7dfe86bf893f0a9be53dc374
DrAndyWest <andrew.west@manchester.ac.uk>
2023-11-11 21:08:42 +0000

    adds urdf and launch folders

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'gz_example_robot_description'
@@ -10,12 +12,16 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Include launch directory
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        # Include URDF directory
+        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*.urdf*')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='sfrvm23',
-    maintainer_email='sfrvm23@todo.todo',
-    description='TODO: Package description',
+    maintainer='DrAndyWest',
+    maintainer_email='andrew.west@manchester.ac.uk',
+    description='Description file for an example two-wheeled robot.',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
