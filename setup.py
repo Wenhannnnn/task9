3bfbc54ba434a04bd86501f51caf4345047c53c7
DrAndyWest <andrew.west@manchester.ac.uk>
2023-11-11 21:27:02 +0000

    adds simple urdf and launch file to view it

--- setup.py
+++ setup.py
@@ -12,10 +12,14 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        # Include launch directory
+        # Include launch files
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
-        # Include URDF directory
-        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*.urdf*')))
+        # Include URDF files
+        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*.urdf*'))),
+        # Include rviz files
+        (os.path.join('share', package_name, 'rviz'), glob(os.path.join('rviz', '*.rviz'))),
+        # Include mesh directory
+        (os.path.join('share', package_name, 'meshes'), glob(os.path.join('meshes', '*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
